defc69af0d471047bcf1971ca493ce75c6132cdf
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-10-24 21:40:58 -0500

    Switch from dependencies to built-ins
    The browser and nodejs have had basic hashing/hmac/randombytes
    operations built in for quite some time. Rather than using these
    packages, which sometimes require extra shims to work in the browser and
    pull in lots of extra code, just rely on the built-ins.

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var SCRAM = require('../');
 
 
 
-test('SCRAM', function (t) {
+test('SCRAM', async function (t) {
     var mech = new SCRAM({
         genNonce: function () {
             return 'MsQUY9iw0T9fx2MUEz6LZPwGuhVvWAhc';
@@ -15,13 +15,13 @@ test('SCRAM', function (t) {
         password: 'secret'
     };
 
-    var initial = mech.response(creds);
+    var initial = await mech.response(creds);
 
     t.equal(initial, 'n,,n=chris,r=MsQUY9iw0T9fx2MUEz6LZPwGuhVvWAhc');
 
-    mech.challenge('r=MsQUY9iw0T9fx2MUEz6LZPwGuhVvWAhc7b276f42-009a-40e2-84ee-8d5c8b206b6a,s=OTFmZGE2ZGQtYjA0Yy00MTRiLTk1ZTktYTkyYWRlMmVkYTc5,i=4096');
+    await mech.challenge('r=MsQUY9iw0T9fx2MUEz6LZPwGuhVvWAhc7b276f42-009a-40e2-84ee-8d5c8b206b6a,s=OTFmZGE2ZGQtYjA0Yy00MTRiLTk1ZTktYTkyYWRlMmVkYTc5,i=4096');
 
-    var res = mech.response(creds);
+    var res = await mech.response(creds);
 
     t.equal(res, 'c=biws,r=MsQUY9iw0T9fx2MUEz6LZPwGuhVvWAhc7b276f42-009a-40e2-84ee-8d5c8b206b6a,p=WKFCdDykcs73+CG653eG721vItw=');
 
